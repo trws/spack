734f6e17b0e45f35e5778ed15f8dec2679fdcc4b
Tom Scogland <scogland1@llnl.gov>
2021-01-21 20:38:09 -0800

    update to work with new kripke cmake setup

--- var/spack/repos/builtin/packages/kripke/package.py
+++ var/spack/repos/builtin/packages/kripke/package.py
@@ -6,50 +6,85 @@
 from spack import *
 
 
-class Kripke(CMakePackage,CudaPackage):
+class Kripke(CMakePackage, CudaPackage):
     """Kripke is a simple, scalable, 3D Sn deterministic particle
        transport proxy/mini app.
     """
+
     homepage = "https://computing.llnl.gov/projects/co-design/kripke"
-    git      = "https://github.com/LLNL/Kripke.git"
+    git = "https://github.com/LLNL/Kripke.git"
 
-    tags = ['proxy-app']
-    version('1.2.4', submodules=True, tag='v1.2.4')
-    version('1.2.3', submodules=True, tag='v1.2.3')
-    version('1.2.2', submodules=True, tag='v1.2.2-CORAL2')
-    version('1.2.1', submodules=True, tag='v1.2.1-CORAL2')
-    version('1.2.0', submodules=True, tag='v1.2.0-CORAL2')
+    tags = ["proxy-app"]
+    version("HEAD", submodules=True, branch="develop")
+    version("1.2.4", submodules=True, tag="v1.2.4")
+    version("1.2.3", submodules=True, tag="v1.2.3")
+    version("1.2.2", submodules=True, tag="v1.2.2-CORAL2")
+    version("1.2.1", submodules=True, tag="v1.2.1-CORAL2")
+    version("1.2.0", submodules=True, tag="v1.2.0-CORAL2")
 
-    variant('mpi',    default=True, description='Build with MPI.')
-    variant('openmp', default=True, description='Build with OpenMP enabled.')
-    variant('caliper', default=False, description='Build with Caliper support enabled.')
-    variant('chai', default=False, description='Build with CHAI/umpire memory management.')
+    variant("mpi", default=True, description="Build with MPI.")
+    variant("openmp", default=True, description="Build with OpenMP enabled.")
+    variant("caliper", default=False, description="Build with Caliper support enabled.")
+    variant(
+        "chai", default=False, description="Build with CHAI/umpire memory management."
+    )
 
-    depends_on('mpi', when='+mpi')
-    depends_on('cmake@3.0:', type='build')
-    depends_on('caliper', when='+caliper')
+    depends_on("mpi", when="+mpi")
+    depends_on("cmake@3.0:", type="build")
+    depends_on("caliper", when="+caliper")
 
-    depends_on('raja')
-    depends_on('raja+cuda', when='+cuda')
-    depends_on('chai', when='+chai')
-    depends_on('chai+cuda', when='+chai+cuda')
+    depends_on("raja")
+    depends_on("raja+cuda", when="+cuda")
+    depends_on("umpire", when="+chai")
+    depends_on("chai", when="+chai")
+    depends_on("chai+cuda", when="+chai+cuda")
 
-    def dfve(self, name, prefix='ENABLE_'):
+    def dfve(self, name, prefix="ENABLE_"):
         return self.define_from_variant(prefix + name.upper(), name)
+
+    def def_prefix_dir(self, name, varname=None):
+        return self.define(
+            (name if varname is None else varname) + "_DIR", self.spec[name].prefix
+        )
+
     def cmake_args(self):
-        return [
-            self.dfve('openmp'),
-            self.dfve('mpi'),
-            self.dfve('caliper'),
-            self.dfve('cuda'),
-            self.dfve('chai'),
+        args = [
+            self.dfve("openmp"),
+            self.dfve("mpi"),
+            self.dfve("caliper"),
+            self.dfve("cuda"),
+            self.dfve("chai"),
+            def_prefix_dir('camp'),
+            def_prefix_dir('raja', varname='RAJA'),
         ]
+        if "+chai" in self.spec:
+            args.append(def_prefix_dir('chai'))
+            args.append(def_prefix_dir('umpire'))
+        return args
 
     def install(self, spec, prefix):
         # Kripke does not provide install target, so we have to copy
         # things into place.
         mkdirp(prefix.bin)
-        install(join_path(self.build_directory, 'bin/kripke.exe'), prefix.bin)
+        install(join_path(self.build_directory, "bin/kripke.exe"), prefix.bin)
 
     def test(self):
-        self.run_test('kripke.exe', options=['--groups', '64', '--gset', '1', '--quad', '128', '--dset', '128', '--legendre', '4', '--zones', '64,32,32', '--procs', '1,1,1'])
+        self.run_test(
+            "kripke.exe",
+            options=[
+                "--groups",
+                "64",
+                "--gset",
+                "1",
+                "--quad",
+                "128",
+                "--dset",
+                "128",
+                "--legendre",
+                "4",
+                "--zones",
+                "64,32,32",
+                "--procs",
+                "1,1,1",
+            ],
+        )
