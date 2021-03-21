e6f2ab71d4869ad79cccdc5163d996ac99f7f390
Harmen Stoppels <harmenstoppels@gmail.com>
2021-03-21 23:31:11 +0100

    abyss: header only

--- var/spack/repos/builtin/packages/abyss/package.py
+++ var/spack/repos/builtin/packages/abyss/package.py
@@ -39,11 +39,6 @@ class Abyss(AutotoolsPackage):
     depends_on('mpi')
     depends_on('boost@:1.50.0,1.53.0:', when='@2.0.2:')
     depends_on('boost@:1.50.0,1.53.0:1.59.0', when='@:1.5.2')
-
-    # TODO: replace this with an explicit list of components of Boost,
-    # for instance depends_on('boost +filesystem')
-    # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants)
     depends_on('sparsehash')
     depends_on('sqlite')
     depends_on('libtool')
