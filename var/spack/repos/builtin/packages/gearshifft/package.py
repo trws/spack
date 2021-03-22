6f0957edbaed26486237c35853dff5b849f18edc
Harmen Stoppels <harmenstoppels@gmail.com>
2021-03-22 09:58:43 +0100

    gearshifft: system, test, program_options, thread

--- var/spack/repos/builtin/packages/gearshifft/package.py
+++ var/spack/repos/builtin/packages/gearshifft/package.py
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-from spack.pkg.builtin.boost import Boost
 
 
 class Gearshifft(CMakePackage):
@@ -39,12 +38,7 @@ class Gearshifft(CMakePackage):
 
     # depends_on C++14 compiler, e.g. GCC 5.0+
     depends_on('cmake@2.8.0:', type='build')
-    depends_on('boost@1.59.0:')
-
-    # TODO: replace this with an explicit list of components of Boost,
-    # for instance depends_on('boost +filesystem')
-    # See https://github.com/spack/spack/pull/22303 for reference
-    depends_on(Boost.with_default_variants)
+    depends_on('boost@1.59.0:+system+test+program_options+thread')
     depends_on('cuda@8.0:', when='+cufft')
     depends_on('opencl@1.2:', when='+clfft')
     depends_on('clfft@2.12.0:', when='+clfft')
